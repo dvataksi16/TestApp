892d58aefefb77bb155d2a0b3711f42a3470760f
Denisa Vataksi <dv758@nyu.edu>
2019-09-18 11:26:34 -0400

    Import test app via file upload

---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '11.0'
+target_swift_version = '5.1'
+
+def shared_pods
+    source 'https://github.com/CocoaPods/Specs.git'
+    use_frameworks!
+end
+
+target 'TestApp' do
+  shared_pods
+end
+
+target 'TestAppTests' do
+  shared_pods
+end
---
+++ TestApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TestApp
+//
+//  Created by Denisa Vataksi on 9/17/19.
+//  Copyright © 2019 Denisa Vataksi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
